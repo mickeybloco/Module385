ee423bf5e531a6e988809e7162ea047ca9aedcfb
mickeybloco <michaelelhines@gmail.com>
2026-09-09 14:43:49 -0500

    Initial Commit

---
+++ 385.1/main.py
@@ -0,0 +1,49 @@
+# Intro to Python
+
+#single line coment - inline comment
+""" multi-
+line
+comment"""
+
+#To Run Python: python3 
+print('Hello, World!')
+
+#variable- you can put whatever you want in variableand you 
+#can call it later in the program.
+
+#primitive date types
+
+#Number :
+# int-integers,whole number discrete
+#float-decimals, complex- complex numbers
+
+"""fav_number = 5
+
+
+#Strings - text,always surrounded by quotes
+
+name = "Mickey"
+lizard_name = 'Syrax'
+text = "joey said \"hello\" to me"
+
+
+#Booleans- true or false values
+speak_german = False
+speak_english = True
+
+print(name,fav_number,lizard_name,text,speak_german,speak_english)
+
+#Formatted strings allow the direct injection of variables /values into a string
+print(f'{name} has a pet named {lizard_name} and his favorite number is {fav_number}.')
+
+"""
+#input() funtion - accepts input from the user
+#in the terminal. To call (iinvoke,run) a function, you must 
+# use parentheses () after the function name.
+name = input('What is your name? ')
+print(f'Hello, {name}!')
+age = input('How old are you? ')
+print(f'You are {age} years old.')
+
+
+
